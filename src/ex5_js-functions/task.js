6a513f6aca50b805aafac38224cac04e02426657
AlekseiMakonovitskii <alexqvod@gmail.com>
2020-10-16 22:20:50 +0300

    Task from ex5_js-functions is done!

---
+++ src/ex5_js-functions/task.js
@@ -0,0 +1,47 @@
+function Calculator() {
+  this.value = 0;
+
+  this.getResult = () => {
+    return this.value;
+  };
+
+  this.reset = () => {
+    this.value = 0;
+  };
+
+  this.add = (num) => {
+    if (typeof num === 'number') {
+      this.value += num;
+    }
+
+    return this.add;
+  };
+
+  this.subtract = (num) => {
+    if (typeof num === 'number') {
+      this.value -= num;
+    }
+
+    return this.subtract;
+  };
+
+  this.divide = (num) => {
+    if (typeof num === 'number') {
+      this.value /= num;
+    }
+
+    return this.divide;
+  };
+
+  this.multiply = (num) => {
+    if (typeof num === 'number') {
+      this.value *= num;
+    }
+
+    return this.multiply;
+  };
+}
+
+let exportCalculator = new Calculator;
+
+module.exports = exportCalculator;
