05f0c8a87f60fe4589be9444b93a51db570feb37
AlekseiMakonovitskii <alexqvod@gmail.com>
2020-12-09 22:40:26 +0300

    Tasks from ex16_js-async are done.

---
+++ src/ex16_js-async/mock.js
@@ -0,0 +1,38 @@
+let candy = [
+  {
+    id: 0,
+    title: 'Bite Size',
+  },
+  {
+    id: 1,
+    title: 'Bon Bons',
+  },
+  {
+    id: 2,
+    title: 'Brittle Candy',
+  },
+  {
+    id: 3,
+    title: 'Bubble Gum',
+  },
+  {
+    id: 4,
+    title: 'Candy Bars',
+  },
+  {
+    id: 5,
+    title: 'Candy Canes',
+  },
+  {
+    id: 6,
+    title: 'Candy Corn',
+  },
+  {
+    id: 7,
+    title: 'Candy Melts',
+  },
+  {
+    id: 8,
+    title: 'Caramels',
+  },
+]
---
+++ src/ex16_js-async/task-02.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('.search');
+
+let debounce = (fn, ms) => {
+  let timeout;
+
+  let call = () => {
+    fn.apply(this ,arguments);
+
+    clearTimeout(timeout);
+    timeout = setTimeout(call, ms);
+  }
+}
+
+let send = () => {
+  let search = candy.find(el => el === input.value);
+
+  if (search) {
+    console.log(search);
+  } else {
+    console.log('Ничего не найдено');
+  }
+}
+
+send = debounce(send, 1000);
+input.addEventListener('keyup', send);
